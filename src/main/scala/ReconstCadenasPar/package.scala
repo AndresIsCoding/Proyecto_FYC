7b6dfe283c978fb9bac91a1881cdd27a3e0a7bf0
MiguelTami <149514850+MiguelTami@users.noreply.github.com>
2023-12-04 19:23:51 -0500

    Comentarios faltantes de función mejorada

--- src/main/scala/ReconstCadenasPar/package.scala
+++ src/main/scala/ReconstCadenasPar/package.scala
@@ -60,56 +60,62 @@ package object ReconstCadenasPar
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     
-  def reconstruirCadenaMejoradoPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
+    def reconstruirCadenaMejoradoPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
 
     // Filtrar el alfabeto para incluir solo las letras que son subcadenas candidatas
     val alfabeto_filtrado = alfabeto.filter(letra => o(Seq(letra)))
     val longitud = n+1
+
     // Función auxiliar para generar todas las posibles secuencias de longitud n en paralelo
     def generarSecuenciasParalelo(n: Int, secuenciasActuales: Seq[Seq[Char]]): Seq[Seq[Char]] = {
+
       // Si n es 1, devuelve las secuencias actuales
       if (n == 1) secuenciasActuales
       else {
-        // Si n es mayor o igual al umbral, genera las secuencias en paralelo
+
+        // Si n es menor o igual al umbral y la secuencia no es generada por una única letra, genera las secuencias en paralelo
         if (n <= longitud - umbral && !(alfabeto_filtrado.length == 1)) {
 
           // Divide el alfabeto filtrado en grupos de 4 letras
           val alfabetoPartes = alfabeto_filtrado.grouped(alfabeto_filtrado.length).toList
+          println(s"AlfabetoPartes: $alfabetoPartes")
 
+          // Utiliza un reconocimiento de patrones para procesar cada parte del alfabeto en paralelo según su longitud
           val resultados = alfabeto_filtrado.length match {
             case 2 =>
+              // Agrega la primera letra del alfabeto filtrado a cada secuencia actual y filtra las secuencias candidatas
               val (res1, res2) = parallel(
                 secuenciasActuales.map(_ :+ alfabetoPartes(0)(0)).filter(o),
                 secuenciasActuales.map(_ :+ alfabetoPartes(0)(1)).filter(o)
               )
+              // Devuelve una tupla con los resultados y dos listas vacías
               (res1, res2, List(), List())
 
             case 3 =>
+              // Agrega la primera letra del alfabeto filtrado a cada secuencia actual y filtra las secuencias candidatas
               val ((res1, res2), res3) = parallel(
-                parallel(
-                  secuenciasActuales.map(_ :+ alfabetoPartes(0)(0)).filter(o),
-                  secuenciasActuales.map(_ :+ alfabetoPartes(0)(1)).filter(o)
-                ),
-                secuenciasActuales.map(_ :+ alfabetoPartes(0)(2)).filter(o)
+                parallel( secuenciasActuales.map(_ :+ alfabetoPartes(0)(0)).filter(o),
+                          secuenciasActuales.map(_ :+ alfabetoPartes(0)(1)).filter(o)),
+                          secuenciasActuales.map(_ :+ alfabetoPartes(0)(2)).filter(o)
               )
+              // Devuelve una tupla con los resultados y una lista vacía
               (res1, res2, res3, List())
 
             case _ =>
+              // Agrega la primera letra del alfabeto filtrado a cada secuencia actual y filtra las secuencias candidatas
               val ((res1, res2), (res3, res4)) = parallel(
                 parallel(
                   secuenciasActuales.map(_ :+ alfabetoPartes(0)(0)).filter(o),
-                  secuenciasActuales.map(_ :+ alfabetoPartes(0)(1)).filter(o)
-                ),
+                  secuenciasActuales.map(_ :+ alfabetoPartes(0)(1)).filter(o)),
                 parallel(
                   secuenciasActuales.map(_ :+ alfabetoPartes(0)(2)).filter(o),
-                  secuenciasActuales.map(_ :+ alfabetoPartes(0)(3)).filter(o)
-                )
+                  secuenciasActuales.map(_ :+ alfabetoPartes(0)(3)).filter(o))
               )
+              // Devuelve una tupla con los resultados
               (res1, res2, res3, res4)
 
           }
-
-          // Descompone la tupla resultados en variables individuales
+          // Descompone la tupla resultados en valores individuales
           val (res1, res2, res3, res4) = resultados
 
           // Combina los resultados de todos los grupos
@@ -121,15 +127,15 @@ package object ReconstCadenasPar
           // Si n es menor que el umbral, genera las secuencias de forma secuencial
           val nuevasSecuencias = for {
             secuenciaActual <- secuenciasActuales
-            //_ = println(s"Secuencia candidata: $secuenciaActual")
             letra <- alfabeto_filtrado
+
+            // Agrega la letra a la secuencia actual para crear una nueva secuencia
             nuevaSecuencia = secuenciaActual :+ letra
-            //_ = println(s"Secuencia actual: $nuevaSecuencia")
 
             // Solo continúa con la recursión si la nueva secuencia es una subcadena candidata
             if o(nuevaSecuencia)
           }
-          yield nuevaSecuencia
+          yield nuevaSecuencia // Devuelve la nueva secuencia
 
           // Llamamos a la función recursivamente con las nuevas secuencias y disminuyendo n en 1
           generarSecuenciasParalelo(n - 1, nuevasSecuencias)
@@ -137,10 +143,11 @@ package object ReconstCadenasPar
       }
     }
 
-    // Generar todas las posibles secuencias de longitud n con valores repetidos
+    // Generar todas las posibles secuencias de longitud n con valores
     val todasLasSecuencias = generarSecuenciasParalelo(n, alfabeto_filtrado.map(Seq(_)))
 
     //Devolvemos la única secuencia candidata de tamaño n que queda luego de terminar la función recursiva
     todasLasSecuencias.head
   }
+
 }
