8184a61350d8a7ddde8adf5ecfe149ca62de0ce1
MiguelTami <149514850+MiguelTami@users.noreply.github.com>
2023-12-07 18:38:40 -0500

    Código actualizado

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -2,7 +2,7 @@ import Oraculo.{Oraculo, alfabeto}
 
 package object ReconstCadenas
 {
-   def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
+     def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
 
     // Función auxiliar para generar todas las posibles secuencias de longitud n
     def generarSecuencias(n: Int, secuenciasActuales: Seq[Seq[Char]]): Seq[Seq[Char]] = {
@@ -10,6 +10,7 @@ package object ReconstCadenas
       // Caso base de la recursión: si n es 1, devuelve las secuencias actuales
       if (n == 1) secuenciasActuales
       else {
+        //println("Llamado")
         // Caso recursivo: para cada secuencia actual y cada letra en el alfabeto, genera todas las secuencias posibles
         // añadiendo la letra a la secuencia actual y disminuyendo n en 1
         val nuevasSecuencias = for {
@@ -31,6 +32,7 @@ package object ReconstCadenas
 
     // Usar el oráculo para verificar cada secuencia y encontrar la primera que pertenece a S
     // Si no se encuentra ninguna, devuelve una secuencia vacía
+    //println(s"Todas las secuencias: $todasLasSecuencias")
     if (todasLasSecuencias.indexWhere(o) == -1) Seq()
     else todasLasSecuencias(todasLasSecuencias.indexWhere(o))
   }
@@ -52,10 +54,10 @@ package object ReconstCadenas
         // añadiendo la letra a la secuencia actual y disminuyendo n en 1
         val nuevasSecuencias = for {
           secuenciaActual <- secuenciasActuales
-          _ = println(s"Secuencia candidata: $secuenciaActual")
+          //_ = println(s"Secuencia candidata: $secuenciaActual")
           letra <- alfabeto_filtrado
           nuevaSecuencia = secuenciaActual :+ letra
-          _ = println(s"Secuencia actual: $nuevaSecuencia")
+          //_ = println(s"Secuencia actual: $nuevaSecuencia")
 
           // Solo continúa con la recursión si la nueva secuencia es una subcadena candidata
           if o(nuevaSecuencia)
@@ -90,7 +92,7 @@ package object ReconstCadenas
       // Si hemos sobrepasado el potencia logN, devolvemos las secuencias que tenemos
       if (potencia > logN) secuencias
       else {
-        println("======================================================================================================")
+       // println("======================================================================================================")
         // Generamos nuevas secuencias concatenando todas las secuencias existentes entre sí
         val nuevasSecuencias = for {
           secuencia1 <- secuencias
@@ -98,12 +100,12 @@ package object ReconstCadenas
           nuevaSecuencia = secuencia1 ++ secuencia2
 
           // Solo continúa con la recursión si la nueva secuencia es una subcadena candidata
-         _ = println(s"Consultando al oráculo: $nuevaSecuencia")
+        // _ = println(s"Consultando al oráculo: $nuevaSecuencia")
           if o(nuevaSecuencia)
         }
         yield nuevaSecuencia
 
-        println(s"Potencia $potencia: Subecuencias candidatas: $nuevasSecuencias")
+       // println(s"Potencia $potencia: Subecuencias candidatas: $nuevasSecuencias")
 
         // Llamamos a la función recursivamente con las nuevas secuencias y aumentamos la potencia
         generarSubsecuencias(nuevasSecuencias, potencia + 1)
@@ -122,7 +124,7 @@ package object ReconstCadenas
 
   //----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  def reconstruirCadenaTurboMejorado(n: Int, o: Oraculo): Seq[Char] = {
+  def reconstruirCadenaTurboMejorada(n: Int, o: Oraculo): Seq[Char] = {
     // Filtrar el alfabeto para incluir solo las letras que son subcadenas candidatas
     val alfabeto_filtrado = alfabeto.filter(letra => o(Seq(letra)))
 
@@ -135,7 +137,7 @@ package object ReconstCadenas
       // Si hemos sobrepasado el potencia logN, devolvemos las secuencias que tenemos
       if (potencia > logN) secuencias
       else {
-          println("======================================================================================================")
+        //println(s"Total secuencias candidatas: ${secuencias.length}")
         // Generamos nuevas secuencias concatenando todas las secuencias existentes entre sí
         val nuevasSecuencias = for {
           secuencia1 <- secuencias
@@ -148,12 +150,12 @@ package object ReconstCadenas
           if secuencias.contains(segundoCuarto ++ tercerCuarto)
 
           // Solo continúa con la recursión si la nueva secuencia es una subcadena candidata
-          _ = println(s"Consultando al oráculo: $nuevaSecuencia")
+         // _ = println(s"Consultando al oráculo: $nuevaSecuencia")
           if o(nuevaSecuencia)
         }
         yield  nuevaSecuencia
 
-        println(s"Potencia $potencia: Subecuencias candidatas: $nuevasSecuencias")
+        //println(s"Potencia $potencia: Subecuencias candidatas: $nuevasSecuencias")
 
         // Llamamos a la función recursivamente con las nuevas secuencias y aumentamos la potencia
         generarSubsecuencias(nuevasSecuencias, potencia + 1)
@@ -162,13 +164,75 @@ package object ReconstCadenas
 
     // Creamos las secuencias iniciales con cada letra del alfabeto que hemos filtrado
     val secuenciasIniciales = alfabeto_filtrado.map(Seq(_))
-    println(s"Secuencias iniciales $secuenciasIniciales")
+   // println(s"Secuencias iniciales $secuenciasIniciales")
 
     // Generamos todas las subsecuencias de tamaño n
     val todasLasSubsecuencias = generarSubsecuencias(secuenciasIniciales, 1)
 
     //Devolvemos la única secuencia candidata de tamaño n que queda luego de terminar la función recursiva
     todasLasSubsecuencias.head
+  }
+//**************************************************************************************************************************
+
+
+  def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
+
+    // Filtrar el alfabeto para incluir solo las letras que son subcadenas candidatas
+    val alfabeto_filtrado = alfabeto.filter(letra => o(Seq(letra)))
+
+    // Calculamos el logaritmo base 2 de n
+    val logN = (Math.log(n) / Math.log(2)).toInt
+
+    def generarSubsecuencias(secuencias: Seq[Seq[Char]], potencia: Int): Seq[Seq[Char]] = {
+
+      // Si hemos sobrepasado el potencia logN, devolvemos las secuencias que tenemos
+      if (potencia > logN) secuencias
+      else {
+        //println("======================================================================================================")
+        // Generamos nuevas secuencias concatenando todas las secuencias existentes entre sí
+        val nuevasSecuencias = for {
+          secuencia1 <- secuencias
+          secuencia2 <- secuencias
+          nuevaSecuencia = secuencia1 ++ secuencia2
+
+          // Solo continúa con la recursión si la nueva secuencia es una subcadena candidata
+          //_ = println(s"Consultando al oráculo: $nuevaSecuencia")
+          if o(nuevaSecuencia)
+        }
+        yield nuevaSecuencia
+
+        //println(s"Potencia $potencia: Subecuencias candidatas: $nuevasSecuencias")
+
+        // Llamamos a la función recursivamente con las nuevas secuencias y aumentamos la potencia
+        generarSubsecuencias(nuevasSecuencias, potencia + 1)
+      }
+    }
+
+    // Creamos las secuencias iniciales con cada letra del alfabeto que hemos filtrado
+    val secuenciasIniciales = alfabeto_filtrado.map(Seq(_))
+    //println(s"Secuencias iniciales $secuenciasIniciales")
+
+    val trie = construirTrie(generarSubsecuencias(secuenciasIniciales, 1))
+
+    println(trie)
+
+    def trieToSeqChar(trie: Trie): Seq[Char] = {
+
+      def dfs(t: Trie, prefix: Seq[Char]): Seq[Char] = {
+        t match {
+          case Nodo(_, _, hijos) =>
+            hijos.flatMap(h => dfs(h, prefix :+ raiz(h)))
+
+          case Hoja(car, _) =>
+            prefix :+ car
+        }
+      }
+
+      dfs(trie, Seq())
+    }
+
+
+    trieToSeqChar(trie).init
   }
 
 }
